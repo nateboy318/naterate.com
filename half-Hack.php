e1f1d22caaba5bc0a651017abb1c0195abf58826
nateboy318 <mcgamer5hundred@gmail.com>
2016-06-14 21:43:22 -0400

    Rename halfHack.php to half-Hack.php

--- half-Hack.php
+++ half-Hack.php
@@ -4,6 +4,11 @@
 <body>
 	<p>get ready for crazy</p>
 	<?php include 'halfHack.php';?>
-
+<?php
+	while (8 > 1){
+		
+		echo "<script>alert('There are no fields to generate a report');</script>";
+	}
+    ?>
     </body>
 </html>
